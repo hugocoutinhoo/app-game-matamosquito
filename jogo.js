46f60e8cc0f10064923ae6e6c9bc0fbc53b8f7e7
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2023-06-14 18:11:25 -0300

    criação da página de game over e ajustes nas regras de vida

--- jogo.js
+++ jogo.js
@@ -1,6 +1,7 @@
 // ------------------------------- dimensionamento da tela realizado
 var altura = 0
 var largura = 0
+var vidas = 1
 
 function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight
@@ -17,7 +18,19 @@ function posicaoRandomica() {
     // remover o mosquito anterior (caso exista)
     if (document.getElementById('mosquito')) {
         document.getElementById('mosquito').remove()
+
+    // Controlando os pontos de vida
+    if (vidas > 3) {
+    // GAME OVER
+        window.location.href = 'fim_de_jogo.html'   
+    } else {
+        document.getElementById('v' + vidas).src="imagens/coracao_vazio.png" 
+        vidas++
     }
+
+    }    
+        
+
     var posicaoX = Math.floor(Math.random() * largura) - 90
     var posicaoY = Math.floor(Math.random() * altura) - 90
 
@@ -34,6 +47,9 @@ function posicaoRandomica() {
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
     mosquito.id = 'mosquito'
+    mosquito.onclick = function() {
+        this.remove()
+    }
 
     document.body.appendChild(mosquito)
 
